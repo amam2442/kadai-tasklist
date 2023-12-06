87c97ca739484c18ba9a62d389cd92914f08243c
yuki.soejima <zimazima2442@gmail.com>
2023-12-06 12:15:18 +0000

    1206tasklist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
-      render :new
+      render root_url
     end
   end
   
